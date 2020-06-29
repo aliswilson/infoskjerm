7966d3731011c34fb71c25b7da2d83991be56c75
Alis Wiken Wilson <alisww@Aliss-MacBook-Pro.local>
2020-06-29 14:18:43 +0200

    add clock

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import Reddit from './Reddit';
 import Bikes from './Bikes';
+import Clock from './Clock';
 
 function App() {
   return (
     <div className="App">
       <h1>Infoskjerm</h1>
+      <Clock />
       <Reddit />
       <Bikes />
       
