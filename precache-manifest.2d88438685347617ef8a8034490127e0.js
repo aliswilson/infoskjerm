ef3a4727d9394f2a8fa4bc48f2112f7f2f173339
Alis Wiken Wilson <alisww@Aliss-MacBook-Pro.local>
2020-06-29 13:42:50 +0200

    Updates

---
+++ precache-manifest.2d88438685347617ef8a8034490127e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c4d14f2c9575289e54abb16f7a77521",
+    "url": "/infoskjerm/index.html"
+  },
+  {
+    "revision": "1e501990d8b2fe5cdc68",
+    "url": "/infoskjerm/static/css/main.b5696f6a.chunk.css"
+  },
+  {
+    "revision": "709ea05894598e6a56e3",
+    "url": "/infoskjerm/static/js/2.2124622a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/infoskjerm/static/js/2.2124622a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e501990d8b2fe5cdc68",
+    "url": "/infoskjerm/static/js/main.8efcca12.chunk.js"
+  },
+  {
+    "revision": "6fa4fe89daa3d5d07bae",
+    "url": "/infoskjerm/static/js/runtime-main.d6c9a5ab.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/infoskjerm/static/media/logo.5d5d9eef.svg"
+  }
+]);
